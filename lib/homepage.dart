68ed390fa6bfb8c7334af48d5f37902ebef6c922
FahimShahriar-Sulfuric <fahimshahriar188@gmail.com>
2023-08-13 21:05:07 +0600

    Completed homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,49 +7,91 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-      body: Column(
-        children: [
-          InkWell(
-            onTap: () {},
-            child: Ink.image(
-                image: AssetImage('assets/Fahim.jpg'),
-              height: 200,
-              width: 100,
-              fit: BoxFit.cover,
-            ),
+        body: Center(
+          child: Wrap(
+            spacing: 20,
+            runSpacing: 20,
+            alignment: WrapAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: null,
+                child: const Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/Alif.jpg'),
+                      radius: 80,
+                    ),
+                    Text(
+                      'Alif',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: null,
+                child: const Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/Avi.jpg'),
+                      radius: 80,
+                    ),
+                    Text(
+                      'Avi',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: null,
+                child: const Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/Fahim.jpg'),
+                      radius: 80,
+                    ),
+                    Text(
+                      'Fahim',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: null,
+                child: const Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/Ajran.jpg'),
+                      radius: 80,
+                    ),
+                    Text(
+                      'Ajran',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: null,
+                child: const Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/Hadia.jpg'),
+                      radius: 80,
+                    ),
+                    Text(
+                      'Hadia',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
-    ),
     );
   }
 }
-
-
-
-// Column(
-// children: [
-// SizedBox(height: 50),
-// Container(
-// child: Row(
-// mainAxisAlignment: MainAxisAlignment.center,
-// children: [
-// Column(
-// children: [
-// CircleAvatar(
-// backgroundImage: AssetImage('assets/Alif.jpg'),
-// radius: 80,
-// ),
-// Text('Alif', style: TextStyle(fontWeight: FontWeight.bold)),
-// ],
-// ),
-// SizedBox(width: 20),
-// CircleAvatar(
-// backgroundImage: AssetImage('assets/Avi.jpg'),
-// radius: 80,
-// ),
-// ],
-// ),
-// ),
-// ],
-// ),
