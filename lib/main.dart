36ad93b427b1ecfc18cd02d309b160cfab96e135
FahimShahriar-Sulfuric <fahimshahriar188@gmail.com>
2023-08-16 22:10:20 +0600

    Changed app name and icon

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,9 @@ class MyApp extends StatelessWidget {
       title: 'Chotto Bondhura',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.light),
-        useMaterial3: true,
-      ),
-      darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.dark),
         useMaterial3: true,
       ),
-      // theme: ThemeData.light(),
-      // darkTheme: ThemeData.dark(),
       home: const LoginPage(),
     );
   }
